0edbd2c23c5d3fca42a516090d4fb6d4af114597
김민경 <minkyle1103@ajou.ac.kr>
2026-01-17 15:30:42 +0900

    Create First_week.java
    자바 프린트 연습 in GDB online

---
+++ First_week.java
@@ -0,0 +1,9 @@
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("안녕"+5.8);//ln을 쓰면 자동줄바꿈임
+		System.out.print(3+5+ "방가" +4+2);//문자열을 만나기 전에는 수학 계산을 하지만 문자열 이후에느 42로 나온다
+		System.out.printf(" %s:%d", "홍길동",15);//f를 붙이면 형식지정출력 
+	}
+}
