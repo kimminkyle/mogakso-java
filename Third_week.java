a63c49629012035267d4a4c4af263f0514547847
김민경 <minkyle1103@ajou.ac.kr>
2026-02-22 14:10:32 +0900

    Third_week.java

--- Third_week.java
+++ Third_week.java
@@ -1,2 +1,27 @@
 public class Third_week{
-  publuc static void main
+  public static void main(String[] args) {
+    int score=72;
+    if (score >=80 && score <90){
+      System.out.println("B학점");
+    }
+    else if (score >=90) {
+      System.out.println("A학점");
+    }
+    else if (score >=70&& score <80){
+      System.out.println("C학점");
+    }
+    else {
+      System.out.println("F학점");
+    }
+    //C와 같은구조
+    for(int j=10;j>0;j-=2){
+        System.out.println(j);
+    }
+    //for문과 같은 while문
+    int k=10;
+    while(k>0){
+        System.out.println(k);
+        k-=2;
+    }
+  }
+}
